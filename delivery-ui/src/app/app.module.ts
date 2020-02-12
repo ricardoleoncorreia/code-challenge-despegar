43b986d2e609af257d44c4199da113bf3d991aa3
Ricardo Leon Correia <ricardoleoncorreia@gmail.com>
2020-02-12 13:25:36 -0300

    Delete DeliveryComponent

--- delivery-ui/src/app/app.module.ts
+++ delivery-ui/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RestaurantsComponent } from './views/restaurants/restaurants.component';
 import { ProductsComponent } from './views/products/products.component';
-import { DeliveryComponent } from './views/delivery/delivery.component';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
@@ -20,7 +19,6 @@ import { ModalModule } from 'ngx-bootstrap/modal';
     AppComponent,
     RestaurantsComponent,
     ProductsComponent,
-    DeliveryComponent,
     PageNotFoundComponent,
     HeaderComponent,
     FooterComponent,
