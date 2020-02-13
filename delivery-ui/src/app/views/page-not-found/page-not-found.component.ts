45a249130576e46edf99adec6def5da5b024f46d
Ricardo Leon Correia <ricardoleoncorreia@gmail.com>
2020-02-13 14:17:08 -0300

    Implement no state in PageNotFound

--- delivery-ui/src/app/views/page-not-found/page-not-found.component.ts
+++ delivery-ui/src/app/views/page-not-found/page-not-found.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationService } from 'src/app/core/services/navigation.service';
+import { DeliveryStateService } from 'src/app/core/services/delivery-state.service';
 
 @Component({
   selector: 'delivery-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss']
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements OnInit {
 
-  constructor(private navigationService: NavigationService) { }
+  constructor(
+    private deliveryStateService: DeliveryStateService,
+    private navigationService: NavigationService) { }
+
+  ngOnInit(): void {
+    this.deliveryStateService.currentPhase$.next(0);
+  }
 
   goToRestaurantsListView(): void {
     this.navigationService.navigateTo('restaurants');
